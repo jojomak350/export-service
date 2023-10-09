efbbcaf7cb4cd4ce0ebced1fd884ca7d38f746c1
Joseph Makram <jojomak350@gmail.com>
2023-10-09 14:20:29 +0300

    WIP

--- handlers/export-handler.go
+++ handlers/export-handler.go
@@ -22,36 +22,6 @@ func ExportHandler(c echo.Context) error {
 		})
 	}
 
-	//client := &http.Client{}
-	//req, _ := http.NewRequest("GET", request.Endpoint, nil)
-	//req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", request.Token))
-	//response, err := client.Do(req)
-	//if err != nil {
-	//	return c.JSON(http.StatusBadRequest, echo.Map{
-	//		"error": err,
-	//	})
-	//}
-
-	//defer response.Body.Close()
-
-	//if response.StatusCode != http.StatusOK {
-	//	return c.JSON(http.StatusBadRequest, echo.Map{
-	//		"status": response.StatusCode,
-	//	})
-	//}
-
-	//data, err := io.ReadAll(response.Body)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//var commentResponse []map[string]interface{}
-
-	//err = json.Unmarshal(data, &commentResponse)
-	//if err != nil {
-	//	panic(err)
-	//}
-
 	file := core.NewFile(request.Filepath)
 
 	file.Generate(request.Header, request.Body)
